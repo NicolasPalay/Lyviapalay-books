e2358ff76af766a78fc087c843ed7983656e04a9
unknown <nicolassam33@gmail.com>
2023-06-22 13:54:11 +0200

    create form inscription

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -46,9 +46,8 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('/'));
+
     }
 
     protected function getLoginUrl(Request $request): string
