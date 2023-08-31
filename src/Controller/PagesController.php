2c369eee0866366dbdbf61219840858255ca5fb9
unknown <nicolassam33@gmail.com>
2023-08-31 14:06:14 +0200

    create pages mentions_legales

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PagesController extends AbstractController
+{
+    #[Route('/mentions-legales', name: 'mentions_legales')]
+    public function mentions(): Response
+    {
+        return $this->render('pages/mentions_legales.html.twig', [
+
+        ]);
+    }
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('pages/mentions.html.twig', [
+
+        ]);
+    }
+}
